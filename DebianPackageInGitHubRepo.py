5470120c9ec94dc8ed074275b4d96f6048c2f6d2
SiRumCZ <sirumcz@gmail.com>
2019-05-28 22:33:32 -0700

    update scripts

--- DebianPackageInGitHubRepo.py
+++ DebianPackageInGitHubRepo.py
@@ -1,3 +1,72 @@
 from github import Github
+import sqlite3
 
-# working in progress
+# persona; access token created from github
+access_token = "***"
+
+pkgdb = "Debian-stable.db"
+ghdb = "Debian-github.db" 
+
+# connect to SQLite db
+ghconn = sqlite3.connect(ghdb)
+ghc = ghconn.cursor()
+
+ghc.execute(''' ATTACH DATABASE \'{}\' AS pkgdb '''.format(pkgdb))
+ghc.execute(
+    '''
+    CREATE TABLE IF NOT EXISTS repos (
+        name text,
+        result text,
+        url text,
+        stars int
+    )
+    '''
+)
+ghc.execute(''' DELETE FROM repos ''')
+
+#
+ghc.execute(''' SELECT DISTINCT name, homepage FROM pkgdb.packages''')
+insert_query = ''' INSERT INTO repos(name, result, url, stars) VALUES(?, ?, ?, ?) '''
+
+gh = Github(access_token)
+
+# https://pygithub.readthedocs.io/en/latest/examples/Repository.html
+index = 0
+for pkg in ghc.fetchall():
+    index += 1
+    name, homepage = pkg
+
+    # if the home page is github url
+    if (homepage is not None and "github.com" in homepage):
+        user, reponame = homepage.strip().split('/')[-2:]
+
+        try:
+            repo = gh.get_repo("{}/{}".format(user, reponame))
+        except Exception:
+            print("cannot get_repo() from homepage of "+name)
+            continue
+
+        ghc.execute(insert_query, (name, None, homepage, repo.stargazers_count))
+        ghconn.commit()
+        continue
+
+    try:
+        repos = gh.search_repositories(query=name)
+    except Exception:
+        print("search_repositories() error")
+        continue
+
+    search_result = (name, None)
+    maxStar = 0
+    firstItem = True
+    for repo in repos:
+        if (repo.stargazers_count > maxStar or firstItem):
+            maxStar = repo.stargazers_count
+            search_result = (repo.name, repo.html_url)
+            firstItem = False
+
+    print("[{}] name: {} result: {} url: {} star: {}".format(index, name, search_result[0], search_result[1], maxStar))
+    ghc.execute(insert_query, (name, search_result[0], search_result[1], maxStar))
+    ghconn.commit()
+
+ghc.close()
---
+++ pullGithubPunRepo.py
@@ -0,0 +1,42 @@
+from github import Github
+from github import GithubException
+import sqlite3
+
+githubdb = "Github-Public-repo.db"
+
+conn = sqlite3.connect(githubdb)
+c = conn.cursor()
+
+c.execute(
+    '''
+    CREATE TABLE IF NOT EXISTS pubrepos (
+        name text,
+        url text,
+        stars int
+    )
+    '''
+)
+c.execute(''' DELETE FROM pubrepos ''')
+
+insert_query = ''' INSERT INTO pubrepos(name, url, stars) VALUES(?, ?, ?) '''
+
+# persona; access token created from github
+access_token = "***"
+
+gh = Github(access_token)
+repos = gh.get_repos()
+
+index = 0
+for repo in repos:
+    index += 1
+    name = repo.name
+    html_url = repo.html_url
+    try:
+        stars = repo.stargazers_count
+    except GithubException as e:
+        stars = 0
+    print("[{}] name: {} url: {} star: {}".format(index, name, html_url, stars))
+    c.execute(insert_query, (name, html_url, stars))
+    conn.commit()
+
+conn.close()
